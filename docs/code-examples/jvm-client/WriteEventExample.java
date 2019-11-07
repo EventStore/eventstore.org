c08980e34136b46189fe52f5219e69f3cbac27e0
Chris Ward <chris.ward@eventstore.org>
2019-11-07 15:12:42 +0100

    Update docs

--- docs/code-examples/jvm-client/WriteEventExample.java
+++ docs/code-examples/jvm-client/WriteEventExample.java
@@ -1,56 +1,42 @@
 package org.eventstore.sample;
 
-import akka.actor.*;
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
-import eventstore.*;
-import eventstore.j.EventDataBuilder;
-import eventstore.j.SettingsBuilder;
-import eventstore.j.WriteEventsBuilder;
-import eventstore.tcp.ConnectionActor;
-
-import java.net.InetSocketAddress;
 import java.util.UUID;
+import akka.actor.*;
+import akka.event.*;
+import eventstore.akka.tcp.ConnectionActor;
+import eventstore.j.*;
+import eventstore.core.*;
 
 public class WriteEventExample {
+
     public static void main(String[] args) {
+
         final ActorSystem system = ActorSystem.create();
-        final Settings settings = new SettingsBuilder()
-                .address(new InetSocketAddress("127.0.0.1", 1113))
-                .defaultCredentials("admin", "changeit")
-                .build();
-        final ActorRef connection = system.actorOf(ConnectionActor.getProps(settings));
+        final ActorRef connection = system.actorOf(ConnectionActor.getProps());
         final ActorRef writeResult = system.actorOf(Props.create(WriteResult.class));
 
-        final EventData event = new EventDataBuilder("event-type")
-                .eventId(UUID.randomUUID())
-                .jsonData("{\"a\": \"1\"}")
-                .build();
+        final EventData event = new EventDataBuilder("my-event").eventId(UUID.randomUUID()).data("my event data")
+                .metadata("my first event").build();
 
-        final WriteEvents writeEvents = new WriteEventsBuilder("newstream")
-                .addEvent(event)
-                .expectAnyVersion()
-                .build();
+        final WriteEvents writeEvents = new WriteEventsBuilder("my-stream").addEvent(event).expectAnyVersion().build();
 
         connection.tell(writeEvents, writeResult);
     }
 
+    public static class WriteResult extends AbstractActor {
 
-    public static class WriteResult extends UntypedActor {
         final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
-        public void onReceive(Object message) throws Exception {
-            if (message instanceof WriteEventsCompleted) {
-                final WriteEventsCompleted completed = (WriteEventsCompleted) message;
-                log.info("range: {}, position: {}", completed.numbersRange(), completed.position());
-            } else if (message instanceof Status.Failure) {
-                final Status.Failure failure = ((Status.Failure) message);
-                final EsException exception = (EsException) failure.cause();
+        @Override
+        public Receive createReceive() {
+            return receiveBuilder().match(WriteEventsCompleted.class, m -> {
+                log.info("range: {}, position: {}", m.numbersRange(), m.position());
+                context().system().terminate();
+            }).match(Status.Failure.class, f -> {
+                final EsException exception = (EsException) f.cause();
                 log.error(exception, exception.toString());
-            } else
-                unhandled(message);
-
-//            context().system().shutdown();
+            }).build();
         }
+
     }
 }
